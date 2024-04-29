83086ce4e720d81136461d4d5d85f418674ed883
Aaby Abhay <aabyabhay@Aabys-MacBook-Air.local>
2024-04-29 07:12:42 +0530

    adding templates

--- Notable/Notable/settings.py
+++ Notable/Notable/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'client/build'],
+            BASE_DIR / 'client/build/index.html'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
