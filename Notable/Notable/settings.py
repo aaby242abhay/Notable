f4b07e0671821973bd0b9347b24a37046cdeb7ca
Aaby Abhay <aabyabhay@Aabys-MacBook-Air.local>
2024-04-29 06:16:07 +0530

    deployment

--- Notable/Notable/settings.py
+++ Notable/Notable/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c($@40bu^cah@q6#g$4vd-th=we4$4g!u=w-z4t!q)3rhwm3a@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
